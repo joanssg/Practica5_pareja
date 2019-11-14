83e0f71949c298ace64825a5d4632d308200dcf2
joan david pichardo hernandez <joanmejor001@hotmail.com>
2019-11-14 15:22:27 -0500

    Prueba

---
+++ Practica5_prog3/Practica5_prog3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica5_prog3.MvcApplication" Language="C#" %>
---
+++ Practica5_prog3/Practica5_prog3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practica5_prog3.Startup))]
+namespace Practica5_prog3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
