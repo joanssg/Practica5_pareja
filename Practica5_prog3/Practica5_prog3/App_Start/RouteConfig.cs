bb2618804904d7abd060eeee6b513d8867912385
joan david pichardo hernandez <joanmejor001@hotmail.com>
2019-11-15 08:35:12 -0500

    Lector funcional sin dise;o

--- Practica5_prog3/Practica5_prog3/App_Start/RouteConfig.cs
+++ Practica5_prog3/Practica5_prog3/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Practica5_prog3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Lector", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Practica5_prog3/Practica5_prog3/Models/Customer_Lector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica5_prog3.Models
+{
+    public class Customer_Lector
+    {
+        public String Id { get;set; }
+        public String Nombre { get; set; }
+        public String Pais { get; set; }
+
+    }
+}
